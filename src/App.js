be9d9c47d53f623b262e98ff309c714e9ec2cd5c
walbagg <lrnmrcl13@gmail.com>
2021-09-19 19:51:44 +0800

    6th

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hackerman
+          lauren
         </p>
         <a
           className="App-link"
