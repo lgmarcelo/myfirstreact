81c61c35166ab5da2fc29817310d3eb5efdda27a
walbagg <lrnmrcl13@gmail.com>
2021-12-14 20:57:11 +0800

    20th

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
                     <a class="navbar-brand" href="/">ICS2608</a>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link" href="/">Home</a>
-                            <a class="nav-link" href="/employees">Employees</a>
-                            <a class="nav-link" href="/add">Add employee</a>
+                            <a class="nav-link" href="/myfirstreact">Home</a>
+                            <a class="nav-link" href="/myfirstreact/employees">Employees</a>
+                            <a class="nav-link" href="/myfirstreact/add">Add employee</a>
                             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
                         </div>
                     </div>
--- src/components/Router.js
+++ src/components/Router.js
@@ -9,11 +9,10 @@ const Router = () => {
         <div>
             <BrowserRouter>
                 <Routes>
-                    <Route exact path="/" element={<Home />} />
                     <Route exact path="/myfirstreact" element={<Home />} />
-                    <Route exact path="/employees" element={<Employee />} />
-                    <Route exact path="/add" element={<AddEmployee />} />
-                    <Route exact path="/edit/:employeeID" element={<AddEmployee />} />
+                    <Route exact path="/myfirstreact/employees" element={<Employee />} />
+                    <Route exact path="/myfirstreact/add" element={<AddEmployee />} />
+                    <Route exact path="/myfirstreact/edit/:employeeID" element={<AddEmployee />} />
                     <Route exact path="*" element={<PageNotFound />} />
                 </Routes>
             </BrowserRouter>
