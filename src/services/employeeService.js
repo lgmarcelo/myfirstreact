6b72a102f1e6653385c8ca1de3550c91f0e51a5c
walbagg <lrnmrcl13@gmail.com>
2021-12-14 20:38:11 +0800

    18th

--- src/services/employeeService.js
+++ src/services/employeeService.js
@@ -1,23 +1,23 @@
 import httpclient from "../commons/httpclient"
 
 const getEmployees =()=>{
-    return httpclient.get('/employees')
+    return httpclient.get('/myfirstreact/employees')
 }
 
 const postEmployee =(data) =>{
-    return httpclient.post('/employees', data)
+    return httpclient.post('/myfirstreact/employees', data)
 }
 
 const putEmployee =(data) =>{
-    return httpclient.put('/employees', data)
+    return httpclient.put('/myfirstreact/employees', data)
 }
 
 const getEmployee =(employeeID)=>{
-    return httpclient.get(`/employees/${employeeID}`)
+    return httpclient.get(`/myfirstreact/employees/${employeeID}`)
 }
 
 const deleteEmployee =(employeeID)=>{
-    return httpclient.delete(`/employees/${employeeID}`)
+    return httpclient.delete(`/myfirstreact/employees/${employeeID}`)
 }
 
 export default {getEmployees, postEmployee, putEmployee, getEmployee, deleteEmployee}
