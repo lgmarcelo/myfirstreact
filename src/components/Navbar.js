506a7a5b9c0db301424cae4c54f97d72036c9572
walbagg <lrnmrcl13@gmail.com>
2021-12-15 11:51:19 +0800

    navbar update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
                     <a class="navbar-brand" href="/">ICS2608</a>
                     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                         <div class="navbar-nav">
-                            <a class="nav-link" href="/myfirstreact">Home</a>
+                            <a class="nav-link" href="/home">Home</a>
                             <a class="nav-link" href="/myfirstreact/employees">Employees</a>
                             <a class="nav-link" href="/myfirstreact/add">Add employee</a>
                             <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
